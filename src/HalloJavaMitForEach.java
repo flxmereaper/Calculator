aada1d78dfd2d398af8067fc1f789739ebf7ca43
flxmereaper <noah4aspoeck@gmail.com>
2024-09-27 14:37:54 +0200

    added class NumberTester and interface NumberTest

--- src/HalloJavaMitForEach.java
+++ src/HalloJavaMitForEach.java
@@ -4,10 +4,14 @@ import java.util.List;
 public class HalloJavaMitForEach {
     public void halloJavaMitForEach() {
         List<String> strings = new ArrayList<>();
+        strings.add("Hello");
+        strings.add("World");
+        strings.add("Java");
+        strings.add("MitForEach");
         for(String s: strings) {
             System.out.println(s);
         }
         strings.forEach(System.out::println);
-        
+
     }
 }
