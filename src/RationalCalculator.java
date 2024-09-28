1adcbbd7f5ed19681b122c2e5ac628b833f92eb5
flxmereaper <noah4aspoeck@gmail.com>
2024-09-28 15:10:03 +0200

    added and finished class RationalCalculator

--- src/RationalCalculator.java
+++ src/RationalCalculator.java
@@ -1,3 +1,55 @@
 public class RationalCalculator extends AbstractCalculator{
+    public RationalCalculator() {
+        super(getAddOperation(), getSubOperation(), getDivOperation(), getMultiplyOperation());
+    }
 
+    @Override
+    public Number add(Number a, Number b) {
+        return add.calc(a, b);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return subtract.calc(a, b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return multiply.calc(a, b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return divide.calc(a, b);
+    }
+    public static CalculateOperation getAddOperation() {
+        return ((x, y) -> {
+            Number result = new Number();
+            result.setA(x.getA()+y.getA());
+            return result;
+        });
+    }
+    public static CalculateOperation getMultiplyOperation() {
+        return ((x, y) ->  {
+            Number result = new Number();
+            result.setA(x.getA()*y.getA());
+            return result;
+        });
+    }
+
+    public static CalculateOperation getDivOperation() {
+        return ((x, y) -> {
+            Number result = new Number();
+            result.setA(x.getA()/y.getA());
+            return result;
+        });
+    }
+
+    public static CalculateOperation getSubOperation() {
+        return ((x, y) -> {
+            Number result = new Number();
+            result.setA(x.getA()-y.getA());
+            return result;
+        });
+    }
 }
