a71860e2be8a87a45b87b3e68188384abdf87728
flxmereaper <noah4aspoeck@gmail.com>
2024-09-28 15:53:27 +0200

    fixed and tested division with complex numbers

--- src/ComplexCalculator.java
+++ src/ComplexCalculator.java
@@ -45,9 +45,11 @@ public class ComplexCalculator extends AbstractCalculator{
             Number cache = new Number();
             cache.setA(y.getA());
             cache.setB(-y.getB());
-            x.setA(x.getA()*y.getA());
-            x.setB(x.getB()*-y.getB());
-            double dividend = y.getA()*y.getA() + y.getB()*y.getB();;
+            double real = x.getA()*cache.getA()-x.getB()*cache.getB(); //Realteil (i² ist -1)
+            double imagine = x.getB()*cache.getA()+x.getA()*cache.getB(); //Imgainärteil
+            x.setA(real);
+            x.setB(imagine);
+            double dividend = y.getA()*y.getA() + y.getB()*y.getB();
             result.setA(x.getA()/dividend);
             result.setB(x.getB()/dividend);
             return result;
